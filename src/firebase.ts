a252cae4abde71ec1d72e88a59ec0f9a6a11719d
luwangdermatologie-stack <luwangdermatologie@gmail.com>
2026-06-25 22:34:31 +0200

    firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -17,12 +17,21 @@ import {
   deleteDoc, 
   writeBatch 
 } from 'firebase/firestore';
-import firebaseConfig from '../firebase-applet-config.json';
 import { Patient, Doctor, ActiveStaff, SystemConfig, TeamsNotification } from './types';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAWYqaDYs5RZ0mndz453VRAcfOIEnUbPWM",
+  authDomain: "fir-test-1-4b2c2.firebaseapp.com",
+  projectId: "fir-test-1-4b2c2",
+  storageBucket: "fir-test-1-4b2c2.firebasestorage.app",
+  messagingSenderId: "614909012167",
+  appId: "1:614909012167:web:bc386e1405e71124b3b6e2",
+  measurementId: "G-91QCXJMQ7V"
+};
 
 const app = initializeApp(firebaseConfig);
 // Keep the database initialized properly
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId); // Cache bust
+export const db = getFirestore(app);
 export const auth = getAuth();
 
 export enum OperationType {
@@ -57,7 +66,8 @@ export function handleFirestoreError(error: unknown, operationType: OperationTyp
     path
   };
   console.error('Firestore Error: ', JSON.stringify(errInfo));
-  throw new Error(JSON.stringify(errInfo));
+  // We remove the throw Error so it doesn't cause uncaught promise rejections 
+  // that completely break the execution flow.
 }
 
 // Preloaded constants for populating empty tables
